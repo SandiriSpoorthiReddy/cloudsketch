72fa7694624a6b758dff9ef535c3dafd3a6e320d
SandiriSpoorthiReddy <sandirispoorthireddy@gmail.com>
2024-11-22 22:01:56 +0530

    Update app.py

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import subprocess
+import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Replace with a secure random key
